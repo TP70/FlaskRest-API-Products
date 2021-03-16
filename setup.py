0492b5b41c91685e32f12076aa47a7f9238c2bad
Thiago Piccioni <thiagopiccioni@gmail.com>
2021-03-16 20:23:57 +0000

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+
+from setuptools import setup
+
+setup(
+    name='Products Flask Api',
+    version='1.0.0',
+    description='Service that stores and retrieve information of a product',
+    author='Thiago Piccioni',
+    author_email='thiagopiccioni@gmail.com',
+
+    classifiers=[
+        'Development Status :: 2 - Prototype/Not Production Ready',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Programming Language :: Python :: 3.9',
+    ],
+
+)
+
+
+
+
+
